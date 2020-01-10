3547ebdecc14e94ff8dfdf97a124ac0f8d21d242
Michael Nicastro <mnicastro11@gmail.com>
2020-01-09 20:42:21 -0500

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,32 @@
+
+import os
+os.system('clear')
+
+full_name = "Michael Nicastro"
+
+print(full_name)
+
+full_name = "bobo"
+print(full_name)
+# my name a jefff
+
+
+
+name = [ "Mike", "hannah", "Luca"]
+print (name)
+
+#tuples
+print (name[0])
+
+fav_pizza = { 
+"Mike": "buffallo",
+"hannah": "cheese",
+"Luca": "Pepperoni"}
+print (fav_pizza["hannah"])
+
+name = True
+print(name)
+
+
+first_name = full_name
+print(first_name)
